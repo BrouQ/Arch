43f3125e99a0c56fa55c387b5b0c9bbf45dbc336
BrouQ <BrouQ@users.noreply.github.com>
2017-05-23 00:08:22 +0200

    Update sw-install.sh

--- sw-install.sh
+++ sw-install.sh
@@ -4,3 +4,4 @@ clear
 
 pacman -Syyu
 pacman -S netcat
+pacman -S gnome-terminal gedit firefox gimp inkscape chromium jwm xorg xorg-xinit xterm xorg-twm xfe xarchiver p7zip zip unzip unrar epdfview gpicview thunar thunar-archive-plugin thunar-volman ttf-ubuntu-font-family mousepad ibam compton vlc qt4 libcdio alsa-utils gnome-calculator gnome-calendar networkmanager network-manager-applet freerdp remmina keepass tlp tlp-rdw youtube-dl putty gparted git openssh openvpn xorg-xbacklight gmrun jre8-openjdk-headless wmctrl owncloud-client gnome-keyring gvfs ntfs-3g lightdm lightdm-gtk-greeter
